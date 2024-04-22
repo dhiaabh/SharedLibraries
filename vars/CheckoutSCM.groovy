d80474b2da3f19aeb0498562de1ef4e45909c4c7
Dhiaa Ben Hamadou <35875892+dhiaabh@users.noreply.github.com>
2024-04-22 11:19:26 +0100

    Update CheckoutSCM.groovy

--- vars/CheckoutSCM.groovy
+++ vars/CheckoutSCM.groovy
@@ -1,22 +1,22 @@
 // File: vars/GitSCM-Checkout.groovy
-def call() {
-        echo "retrieved!"
-}
+// def call() {
+//         echo "retrieved!"
+// }
 // def call(url, depth, branch, gitCredentials) {
-// def call(url, depth, branch) {
+def call(url, depth, branch) {
 //         // def credentials = gitCredentials("$gitCredentials")
 //         // def username = credentials.username
 //         // def password = credentials.password
         
-//         def parts = url.split("://", 2)
-//         def protocol = parts[0] + "://"
-//         def gitRepository = parts[1]
+        def parts = url.split("://", 2)
+        def protocol = parts[0] + "://"
+        def gitRepository = parts[1]
         
 //         // sh "git clone --depth 1 --branch ${branch} ${protocol}${username}:${password}@${gitRepository} ."
 
-//         sh "echo ${protocol}"
-//         sh "echo ${gitRepository}"
-//         sh "echo ${depth}"
-//         sh "echo ${branch}"
+        sh "echo ${protocol}"
+        sh "echo ${gitRepository}"
+        sh "echo ${depth}"
+        sh "echo ${branch}"
 
-// }
+}
