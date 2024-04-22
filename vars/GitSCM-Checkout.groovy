eb7f3b682a5d3f005b60711e45f2a7da925fdfb6
Dhiaa Ben Hamadou <35875892+dhiaabh@users.noreply.github.com>
2024-04-22 09:51:28 +0100

    add files

---
+++ vars/GitSCM-Checkout.groovy
@@ -0,0 +1,13 @@
+// File: vars/GitSCM-Checkout.groovy
+
+def call(url, depth, branch, gitCredentials) {
+        def credentials = gitCredentials("$gitCredentials")
+        def username = credentials.username
+        def password = credentials.password
+        
+        def parts = url.split("://", 2)
+        def protocol = parts[0] + "://"
+        def gitRepository = parts[1]
+        
+        sh "git clone --depth ${depth} --branch ${branch} ${protocol}${username}:${password}@${gitRepository} ."
+}
