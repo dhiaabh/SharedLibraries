bafee72f3e281b65b3d18c7373e4a44821b10b64
Dhiaa Ben Hamadou <35875892+dhiaabh@users.noreply.github.com>
2024-04-22 10:37:05 +0100

    Update GitSCM-Checkout.groovy

--- vars/GitSCM-Checkout.groovy
+++ vars/GitSCM-Checkout.groovy
@@ -1,20 +1,22 @@
 // File: vars/GitSCM-Checkout.groovy
-
-// def call(url, depth, branch, gitCredentials) {
 def call(url, depth, branch) {
-        // def credentials = gitCredentials("$gitCredentials")
-        // def username = credentials.username
-        // def password = credentials.password
+        sh " echo retrieved! "
+}
+// def call(url, depth, branch, gitCredentials) {
+// def call(url, depth, branch) {
+//         // def credentials = gitCredentials("$gitCredentials")
+//         // def username = credentials.username
+//         // def password = credentials.password
         
-        def parts = url.split("://", 2)
-        def protocol = parts[0] + "://"
-        def gitRepository = parts[1]
+//         def parts = url.split("://", 2)
+//         def protocol = parts[0] + "://"
+//         def gitRepository = parts[1]
         
-        // sh "git clone --depth 1 --branch ${branch} ${protocol}${username}:${password}@${gitRepository} ."
+//         // sh "git clone --depth 1 --branch ${branch} ${protocol}${username}:${password}@${gitRepository} ."
 
-        sh "echo ${protocol}"
-        sh "echo ${gitRepository}"
-        sh "echo ${depth}"
-        sh "echo ${branch}"
+//         sh "echo ${protocol}"
+//         sh "echo ${gitRepository}"
+//         sh "echo ${depth}"
+//         sh "echo ${branch}"
 
 }
